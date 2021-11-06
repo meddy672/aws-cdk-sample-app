2bed7c07816ddfb81afcaa1ed56d5db6f8fa5ade
Matthew Eddy <meddy672@gmail.com>
2021-11-06 13:47:54 -0400

    added hit construct to the stack

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -13,8 +14,12 @@ export class CdkWorkshopStack extends cdk.Stack {
       handler: 'hello.handler' // file is "hello", function is "handler"
     });
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: helloWithCounter.handler
     });
 
   }
