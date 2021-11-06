525744331d65e38031a364664c593cad7ad2ff94
Matthew Eddy <meddy672@gmail.com>
2021-11-06 12:54:13 -0400

    added lambda and api gateway constructs

---
+++ lib/hitcounter.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+
+export interface HitCounterProps {
+    downstream: lambda.IFunction;
+}
+
+export class HitCounter extends cdk.Construct {
+    constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+        super(scope, id);
+
+        // TODO: Write code here
+    }
+}
