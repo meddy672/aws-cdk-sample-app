df0747ea02c3180df376d4fd082715087c8d2e17
Matthew Eddy <meddy672@gmail.com>
2021-11-06 19:03:00 -0400

    added test case

--- test/cdk-workshop.test.ts
+++ test/cdk-workshop.test.ts
@@ -1,21 +1,12 @@
 import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as CdkWorkshop from '../lib/cdk-workshop-stack';
 
-test('SQS Queue Created', () => {
+test('Hello Lambda Created', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
-    }));
-});
-
-test('SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
-  // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+    expectCDK(stack).to(haveResource("AWS::Lambda::Function"));
 });
