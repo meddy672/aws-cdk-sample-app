83f448a610f9f25028816231381444d5efd11dc2
Matthew Eddy <meddy672@gmail.com>
2021-11-06 19:13:17 -0400

    updated test cases

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -3,7 +3,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import { HitCounter }  from '../lib/hitcounter';
 
-test('DynamoDB Table Created', () => {
+test('DynamoDB Table Created With Encryption', () => {
   const stack = new cdk.Stack();
   // WHEN
   new HitCounter(stack, 'MyTestConstruct', {
@@ -14,7 +14,42 @@ test('DynamoDB Table Created', () => {
     })
   });
   // THEN
-
   const template = Template.fromStack(stack);
-  template.resourceCountIs("AWS::DynamoDB::Table", 1);
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    SSESpecification: {
+      SSEEnabled: true
+    }
+  });
 });
+
+test('Lambda Has Environment Variables', () => {
+  const stack = new cdk.Stack();
+  // WHEN
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream:  new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
+  // THEN
+  const template = Template.fromStack(stack);
+  const envCapture = new Capture();
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: envCapture,
+  });
+
+  expect(envCapture.asObject()).toEqual(
+    {
+      Variables: {
+        DOWNSTREAM_FUNCTION_NAME: {
+          Ref: "TestFunction22AD90FC",
+        },
+        HITS_TABLE_NAME: {
+          Ref: "MyTestConstructHits24A357F0",
+        },
+      },
+    }
+  );
+});
+
